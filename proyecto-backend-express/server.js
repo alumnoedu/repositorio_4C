2e3da4325fcbfb6be3ae2b36385cf0ea2feb75aa
unknown <irmaromero478@gmail.com>
2023-09-07 12:44:49 -0600

    Inicializa el proyecto con express.js

---
+++ proyecto-backend-express/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.get('/',(req,res) =>{
+    res.send('!Servidor Express funcionando en el puerto' + port + '!');
+});
+
+app.listen(port,() => {
+    console.log('Servidor en funcionamiento en http://localhost:' + port);
+});
